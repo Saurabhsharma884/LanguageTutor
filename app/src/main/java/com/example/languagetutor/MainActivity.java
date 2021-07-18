15301dd68d64bdf5c8f55cd21651d2c6a8b9d9dc
Saurabh Sharma <saurabh182000@gmail.com>
2021-07-18 12:38:46 +0530

    Initial Commit

--- app/src/main/java/com/example/languagetutor/MainActivity.java
+++ app/src/main/java/com/example/languagetutor/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.languagetutor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.languagetutor.indianLang.HindiActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView hindiView = findViewById(R.id.indian_view);
+        CardView germanView = findViewById(R.id.german_view);
+        CardView spanishView = findViewById(R.id.spanish_view);
+        CardView frenchView = findViewById(R.id.french_view);
+        CardView koreanView = findViewById(R.id.korean_view);
+        CardView japanView = findViewById(R.id.japan_view);
+
+        hindiView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,HindiActivity.class);
+                intent.putExtra("language","HINDI");
+                startActivity(intent);
+            }
+        });
+
+        germanView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        spanishView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        frenchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        koreanView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        japanView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
